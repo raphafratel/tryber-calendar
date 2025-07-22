f78143f4cdf61b08aac030b69c66ceef00c4c7fa
Raphael Fratel <raphael.fratel@gmail.com>
2025-07-22 10:16:02 -0300

    Implementa eventListener para alterar fridays e holidays

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-
 const createDaysOfTheWeek = () => {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDays = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ];
   const weekDaysList = document.querySelector('.week-days');
 
   for (let index = 0; index < weekDays.length; index += 1) {
@@ -9,42 +16,62 @@ const createDaysOfTheWeek = () => {
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-}
+  }
+};
 
 createDaysOfTheWeek();
 
-const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const decemberDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
 
 // Escreva seu código abaixo.
-let ulDays = document.getElementById('days');
+const ulDays = document.getElementById('days');
 
 for (let index = 0; index < decemberDaysList.length; index += 1) {
-  let li = document.createElement('li');
+  const li = document.createElement('li');
   li.innerText = decemberDaysList[index];
   li.className = 'day';
   ulDays.appendChild(li);
 
-  if (index === 25/*dia 24*/  || index === 26/*dia 25*/ || index === 32/*dia 31*/) {
+  if (index === 25 || index === 26 || index === 32) {
     li.classList.add('holiday');
   }
 
-  if (index === 5/*dia 4*/ || index === 12/*dia 11*/ || index === 19/*dia 18*/ || index === 26/*dia 25*/) {
+  if (index === 5 || index === 12 || index === 19 || index === 26) {
     li.classList.add('friday');
   }
 }
 
-let holidays = document.querySelectorAll('.holiday');
 const btnHoliday = document.getElementById('btn-holiday');
 
 btnHoliday.addEventListener('click', () => {
+  const holidays = document.querySelectorAll('.holiday');
   for (let index = 0; index < holidays.length; index += 1) {
-    const currentColor = getComputedStyle(holidays[index]).backgroundColor;
-
-    if (currentColor === 'rgb(238, 238, 238)') {
-      holidays[index].style.backgroundColor = 'red';
-    } else {
+    if (holidays[index].style.backgroundColor === 'red') {
       holidays[index].style.backgroundColor = 'rgb(238, 238, 238)';
+    } else {
+      holidays[index].style.backgroundColor = 'red';
+    }
+  }
+});
+
+const btnFriday = document.getElementById('btn-friday');
+const fridays = document.querySelectorAll('.friday');
+
+/* Criei um array para armazenar o estado original do innerText de cada elemento friday */
+const backupFridays = [];
+for (let i = 0; i < fridays.length; i += 1) {
+  backupFridays.push(fridays[i].innerText);
+}
+
+btnFriday.addEventListener('click', () => {
+  for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerText === 'red') {
+      fridays[index].innerText = backupFridays[index];
+    } else {
+      fridays[index].innerText = 'red';
     }
   }
 });
